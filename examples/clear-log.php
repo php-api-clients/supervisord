5fd4acf399f977e4d73b1562514bdedb8ab978d6
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-01-27 23:01:02 +0100

    CS fix

--- examples/clear-log.php
+++ examples/clear-log.php
@@ -6,7 +6,6 @@ require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 
 $client = Client::create(require __DIR__ . DIRECTORY_SEPARATOR . 'resolve_host.php');
 
-
 if ($client->clearLog()) {
     echo 'log cleared', PHP_EOL;
 
