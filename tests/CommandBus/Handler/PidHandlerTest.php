1866632770f5c6450e028d2265e7a9b006aec4ff
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-01-29 21:02:27 +0100

    PidHandler test

---
+++ tests/CommandBus/Handler/PidHandlerTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Tests\Supervisord\CommandBus\Handler;
+
+use ApiClients\Client\Supervisord\CommandBus\Command\PidCommand;
+use ApiClients\Client\Supervisord\CommandBus\Handler\PidHandler;
+use ApiClients\Tools\Services\XmlRpc\XmlRpcService;
+use ApiClients\Tools\TestUtilities\TestCase;
+use function React\Promise\resolve;
+
+final class PidHandlerTest extends TestCase
+{
+    public function testCommand()
+    {
+        $service = $this->prophesize(XmlRpcService::class);
+        $service->call('supervisor.getPID')->shouldBeCalled()->willReturn(resolve(123456));
+
+        $handler = new PidHandler($service->reveal());
+        $version = $this->await($handler->handle(new PidCommand()));
+
+        self::assertSame(123456, $version);
+    }
+}
